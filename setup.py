80ba66339dff9f138cb0a8da0341632b0825641d
Oliver <oliver.limoyo@mail.mcgill.ca>
2024-01-11 14:31:47 -0500

    Fix requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 # TODO: see https://github.com/pymanopt/pymanopt/blob/master/setup.py for mmore later
 setup(
     name="generative-graphik",
-    version="0.01",
+    version="0.1",
     description="Generative inverse kinematics",
-    author="Some dude",
+    author="Filip Maric, Oliver Limoyo",
     author_email="filip.maric@robotics.utias.utoronto.ca, oliver.limoyo@robotics.utias.utoronto.ca",
     license="MIT",
     url="https://github.com/utiasSTARS/generative-graphik",
@@ -15,7 +15,7 @@ setup(
         "urdfpy",
         "numpy >= 1.16",
         "liegroups @ git+ssh://git@github.com/utiasSTARS/liegroups@generative_ik#egg=liegroups",
-        "graphIK @ git+ssh://git@github.com/utiasSTARS/graphIK.git",
+        "graphIK @ git+ssh://git@github.com/utiasSTARS/graphIK@generative_ik#egg=graphik",
         "networkx >= 2.8.7",
         "tensorboard"
     ],
