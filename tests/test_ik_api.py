61c626d3909beea413b6c5f86e49cf037503d1cd
Jonathan Külz <jonathan.kuelz@tum.de>
2024-03-07 19:01:46 +0100

    start multiprocessing implementation

--- tests/test_ik_api.py
+++ tests/test_ik_api.py
@@ -1,13 +1,13 @@
 import itertools
 from time import time
-from typing import Tuple
+from typing import List, Sequence, Tuple
 import unittest
 
 import numpy as np
 import torch
 
 from generative_graphik.utils.dataset_generation import random_revolute_robot_graph
-from generative_graphik.utils.api import ik
+from generative_graphik.utils.api import ik, ik_mp
 from generative_graphik.utils.get_model import get_model
 from generative_graphik.utils.torch_to_graphik import joint_transforms_from_t_zeros, joint_transforms_to_t_zero
 
@@ -16,6 +16,10 @@ class ApiTests(unittest.TestCase):
     """
     Tests the generative_graphik.utils.api functionalities.
     """
+    
+    dof = 6
+    nR = 16
+    nG = 4
 
     def setUp(self):
         """
@@ -34,6 +38,18 @@ class ApiTests(unittest.TestCase):
                 raise FileNotFoundError("No model found. Train a model and place it in the directory specified in the "
                                         "config.yaml file.")
 
+        self.graphs = [random_revolute_robot_graph(self.dof) for _ in range(self.nR)]
+        goals = dict.fromkeys(range(self.nR), None)
+        for i, j in itertools.product(range(self.nR), range(self.nG)):
+            if j == 0:
+                goals[i] = []
+            q = torch.rand(self.dof + 1) * 2 * torch.pi - torch.pi
+            angles = {jnt: q_jnt.item() for jnt, q_jnt in zip(self.graphs[i].robot.joint_ids, q)}
+            T = self.graphs[i].robot.pose(angles, self.graphs[i].robot.end_effectors[-1])
+            goals[i].append(torch.Tensor(T.as_matrix()).to(self.device))
+        self.goals = goals
+        self.goals_tensor = torch.stack([torch.stack(self.goals[key]) for key in sorted(goals.keys())])
+
     @staticmethod
     def ik_error(T_desired: torch.Tensor, T_eef: torch.Tensor) -> Tuple[float, float]:
         """Utility function to compute the error between two SE3 matrices."""
@@ -45,51 +61,57 @@ class ApiTests(unittest.TestCase):
         e_trans = torch.norm(t_desired - t_eef)  # meters
         return e_rot.item(), e_trans.item()
 
-    def test_conversions(self, N=10, dof=6):
+    def eval_full_ik(self, q: torch.Tensor) -> Tuple[List[float], List[float]]:
+        if len(q.shape) == 3:
+            q = torch.unsqueeze(q, 2)
+        samples = q.shape[2]
+        trans_errors, rot_errors = list(), list()
+        for i in range(self.nR):
+            g = self.graphs[i]
+            for k, l in itertools.product(range(self.nG), range(samples)):
+                q_kl = {jnt: q[i, k, l, m].item() for m, jnt in enumerate(g.robot.joint_ids[1:])}
+                q_kl['p0'] = 0
+                T = g.robot.pose(q_kl, g.robot.end_effectors[-1]).as_matrix()
+                e_rot, e_trans = self.ik_error(self.goals[i][k], torch.tensor(T, device=self.device, dtype=torch.float32))
+                rot_errors.append(e_rot)
+                trans_errors.append(e_trans)
+        return trans_errors, rot_errors
+
+    def test_conversions(self, N=10):
         """Test that the joint transforms (torch) can be converted to T_zero (dict of SE3) and back."""
         for _ in range(N):
-            g = random_revolute_robot_graph(dof)
+            g = random_revolute_robot_graph(self.dof)
             T_zero = g.robot.from_dh_params(g.robot.params)
             transforms = joint_transforms_from_t_zeros(T_zero, keys=g.robot.joint_ids, device=self.device)
             T_zero_reconstructed = joint_transforms_to_t_zero(transforms, keys=g.robot.joint_ids)
             for key in T_zero:
                 self.assertTrue(np.allclose(T_zero[key].as_matrix(), T_zero_reconstructed[key].as_matrix()))
 
-    def test_ik_api(self, nR: int = 32, nG: int = 16, samples: int = 32, dof: int = 6):
+    def test_ik_api(self, samples: int = 32):
         """
         Test the inverse kinematics API, i.e., an inverse kinematics functionality that is framework-agnostic and does
         not require the user to know the details of the generative_graphik approach.
         """
         tic = time()
         t_eval = 0
-        graphs = [random_revolute_robot_graph(dof) for _ in range(nR)]
-        goals = dict.fromkeys(range(nR), None)
-        for i, j in itertools.product(range(nR), range(nG)):
-            if j == 0:
-                goals[i] = []
-            q = torch.rand(dof + 1) * 2 * torch.pi - torch.pi
-            angles = {jnt: q_jnt.item() for jnt, q_jnt in zip(graphs[i].robot.joint_ids, q)}
-            T = graphs[i].robot.pose(angles, graphs[i].robot.end_effectors[-1])
-            goals[i].append(torch.Tensor(T.as_matrix()).to(self.device))
-
         all_cost = list()
         best_cost = list()
-        for i, g in enumerate(graphs):
+        for i, g in enumerate(self.graphs):
             T_zero_native = g.robot.from_dh_params(g.robot.params)
             transforms = joint_transforms_from_t_zeros(T_zero_native, keys=g.robot.joint_ids, device=self.device)
             transforms = torch.unsqueeze(transforms, 0)
-            sol = ik(transforms, torch.unsqueeze(torch.stack(goals[i]), dim=0), samples=samples, return_all=True)
+            sol = ik(transforms, torch.unsqueeze(torch.stack(self.goals[i]), dim=0), samples=samples, return_all=True)
 
             t_eval_start = time()
 
             trans_errors = list()
             rot_errors = list()
 
-            for k, l in itertools.product(range(nG), range(samples)):
+            for k, l in itertools.product(range(self.nG), range(samples)):
                 q_kl = {jnt: sol[0, k, l, m].item() for m, jnt in enumerate(g.robot.joint_ids[1:])}
                 q_kl['p0'] = 0
                 T = g.robot.pose(q_kl, g.robot.end_effectors[-1]).as_matrix()
-                e_rot, e_trans = self.ik_error(goals[i][k], torch.tensor(T, device=self.device, dtype=torch.float32))
+                e_rot, e_trans = self.ik_error(self.goals[i][k], torch.tensor(T, device=self.device, dtype=torch.float32))
                 rot_errors.append(e_rot)
                 trans_errors.append(e_trans)
 
@@ -102,17 +124,42 @@ class ApiTests(unittest.TestCase):
             # Is it significantly better than random? (educated guess of what a random precision would be)
             self.assertLessEqual(np.mean(rot_errors), 45)
             self.assertLessEqual(np.min(trans_errors),
-                                 np.mean([np.linalg.norm(goals[i][j][:3, 3].detach().cpu().numpy()) for j in range(nG)]) / 10)
+                                 np.mean([np.linalg.norm(self.goals[i][j][:3, 3].detach().cpu().numpy()) for j in range(self.nG)]) / 10)
             best_cost.append(np.min(cost))
             all_cost.append(np.mean(cost))
             t_eval += time() - t_eval_start
 
         toc = time()
         delta_t = toc - tic - t_eval
-        print(f"\n\nIK Test took {delta_t:.2f} seconds. That's {1000 * delta_t / (nR * nG):.2f} ms per goal "
-              f"or {1000 * delta_t / nR:.2f} ms per robot.")
+        print(f"\n\nIK Test took {delta_t:.2f} seconds. That's {1000 * delta_t / (self.nR * self.nG):.2f} ms per goal "
+              f"or {1000 * delta_t / self.nR:.2f} ms per robot.")
         print(f"Mean best: {np.mean(best_cost):.4f}, Std: {np.std(best_cost):.4f}")
         print(f"Mean cost: {np.mean(all_cost):.3f}, Std: {np.std(all_cost):.3f}")
+
+    def test_multi_ik(self, samples: int = 32):
+        all_transforms = torch.zeros((self.nR, self.dof, 4, 4), device=self.device)
+
+        for i, g in enumerate(self.graphs):
+            T_zero_native = g.robot.from_dh_params(g.robot.params)
+            all_transforms[i, ...] = joint_transforms_from_t_zeros(T_zero_native, keys=g.robot.joint_ids, device=self.device)
+
+        results = dict()
+        tic = time()
+        results['normal'] = ik(all_transforms, self.goals_tensor, samples=samples)
+        toc = time()
+        results['mp'] = ik_mp(all_transforms, self.goals_tensor, samples=samples)
+        toc_mp = time()
+        times = {'normal': toc - tic, 'mp': toc_mp - toc}
+
+        for key in results:
+            trans, rot = self.eval_full_ik(results[key])
+            cost = [trans + rot * 0.05 / 2 for trans, rot in zip(trans, rot)]
+            delta_t = times[key]
+            print(key)
+            print(f"\n\nIK only took {delta_t:.2f} seconds. That's {1000 * delta_t / (self.nR * self.nG):.2f} ms per goal "
+                  f"or {1000 * delta_t / self.nR:.2f} ms per robot.")
+            print(f"Mean cost: {np.mean(cost):.4f}, Std: {np.std(cost):.4f}")
+            self.assertTrue(np.mean(cost) < 0.075)
 
 
 if __name__ == '__main__':
